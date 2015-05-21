6f7deb1cb24e057d1b4ec001b530b642abd0fddf
Andrew Berry <deviantintegral@gmail.com>
2015-05-21 13:08:04 -0400

    Fix invalid path for xhgui templates.

--- php5/xhgui/config.php
+++ php5/xhgui/config.php
@@ -17,7 +17,7 @@ return array(
 
     // Allows you to pass additional options like replicaSet to MongoClient.
     'db.options' => array(),
-    'templates.path' => dirname(__DIR__) . '/src/templates',
+    'templates.path' => '/opt/xhgui/src/templates',
     'date.format' => 'M jS H:i:s',
     'detail.count' => 6,
     'page.limit' => 25,
